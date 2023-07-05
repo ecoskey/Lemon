15d48a3e9150d1e0ff949229d43c055fd0459f75
Jackson Roberts <jacksongroberts@protonmail.com>
2023-07-04 17:33:46 -0700

    finished redoing ender chest recipes
    now just need assets 💀

--- modpack/overrides/kubejs/server_scripts/enderchest_recipes.js
+++ modpack/overrides/kubejs/server_scripts/enderchest_recipes.js
@@ -5,67 +5,138 @@ onEvent('recipes', event => {
     event.remove({ output: EC('ender_chest') });
     event.remove({ output: EC('ender_bag') });
 
-    event.shaped(
-        Item.of(KJ('blue_portal_component'), 1),
-        [
-            'DBD',
-            'BPB',
-            'DBD'
+    event.custom({
+        type: CR('mechanical_crafting'),
+        pattern: [
+            'BMB',
+            'S S',
         ],
-        {
-            D: MC('diamond'),
-            B: MC('blue_dye'),
-            P: MC('ender_pearl')
-        }
-    );
+        key: {
+            'B': { item: CR('brass_ingot') },
+            'M': { item: CR('precision_mechanism') },
+            'S': { item: CR('sturdy_sheet') }
+        },
+        result: { item: KJ('top_dimensional_casing') },
+        acceptMirrored: true
+    })
 
-    event.shaped(
-        Item.of(KJ('orange_portal_component'), 1),
-        [
-            'DOD',
-            'OPO',
-            'DOD'
+    event.custom({
+        type: CR('mechanical_crafting'),
+        pattern: [
+            'S S',
+            'ENE',
         ],
-        {
-            D: MC('diamond'),
-            O: MC('orange_dye'),
-            P: MC('ender_pearl')
-        }
-    );
+        key: {
+            'E': { item: CR('electron_tube') },
+            'N': { item: MC('netherite_ingot') },
+            'S': { item: CR('sturdy_sheet') }
+        },
+        result: { item: KJ('bottom_dimensional_casing') },
+        acceptMirrored: true
+    })
 
-    /* let portalCoreIntermediate = KJ('incomplete_portal_core';
-    event.recipes.createSequencedAssembly(
-        KJ('portal_core',
-        CR('sturdy_sheet',
-        [
-            event.recipes.createDeploying(portalCoreIntermediate, [portalCoreIntermediate, KJ('blue_portal_component']),
-            event.recipes.createDeploying(portalCoreIntermediate, [portalCoreIntermediate, KJ('orange_portal_component']),
-            event.recipes.createPressing(portalCoreIntermediate, portalCoreIntermediate)
+    event.custom({
+        type: CR('mixing'),
+        ingredients: [
+            {
+                item: MC('sugar')
+            },
+            {
+                item: MC('sugar')
+            },
+            {
+                item: CR('wheat_flour')
+            },
+            {
+                fluid: MC('water'),
+                amount: 250
+            }
+        ],
+        results: [
+            {
+                item: KJ('graham_cracker'),
+                count: 4
+            }
+        ],
+        heatRequirement: 'heated'
+    })
+
+    event.custom({
+        type: CR('mixing'),
+        ingredients: [
+            {
+                fluid: 'create_confectionery:black_chocolate',
+                amount: 250
+            },
+            {
+                item: MC('black_concrete_powder')
+            },
+            {
+                item: CR('powdered_obsidian')
+            }
+        ],
+        results: [
+            {
+                fluid: KJ('darkest_chocolate'),
+                amount: 125
+            }
+        ],
+        heatRequirement: 'superheated'
+    })
+
+    event.custom({
+        type: CR('compacting'),
+        ingredients: [
+            {
+                fluid: KJ('darkest_chocolate'),
+                amount: 250
+            }
+        ],
+        results: [
+            {
+                item: KJ('bar_of_darkest_chocolate')
+            }
         ]
-    ).transitionalItem(portalCoreIntermediate).loops(1); */
+    })
+
+    event.custom({
+        type: CR('mixing'),
+        ingredients: [
+            {
+                item: KJ('bar_of_darkest_chocolate')
+            }
+        ],
+        results: [
+            {
+                fluid: KJ('darkest_chocolate'),
+                amount: 250
+            }
+        ],
+        heatRequirement: 'superheated'
+    })
 
     event.custom({
         type: CR('sequenced_assembly'),
         ingredient: {
-            item: CR('sturdy_sheet')
+            item: KJ('graham_cracker')
         },
         transitionalItem: {
-            item: KJ('incomplete_portal_core')
+            item: KJ('incomplete_smingularity')
         },
         sequence: [
             {
                 type: CR('deploying'),
                 ingredients: [
                     {
-                        item: KJ('incomplete_portal_core')
+                        item: KJ('incomplete_smingularity')
                     },
                     {
-                        item: KJ('blue_portal_component')
+                        item: 'create_confectionery:marshmallow'
                     }
                 ],
                 results: [
                     {
-                        item: KJ('incomplete_portal_core')
+                        item: KJ('incomplete_smingularity')
                     }
                 ]
             },
@@ -73,15 +144,31 @@ onEvent('recipes', event => {
                 type: CR('deploying'),
                 ingredients: [
                     {
-                        item: KJ('incomplete_portal_core')
+                        item: KJ('incomplete_smingularity')
                     },
                     {
-                        item: KJ('orange_portal_component')
+                        item: KJ('bar_of_darkest_chocolate')
                     }
                 ],
                 results: [
                     {
-                        item: KJ('incomplete_portal_core')
+                        item: KJ('incomplete_smingularity')
+                    }
+                ]
+            },
+            {
+                type: CR('deploying'),
+                ingredients: [
+                    {
+                        item: KJ('incomplete_smingularity')
+                    },
+                    {
+                        item: KJ('graham_cracker')
+                    }
+                ],
+                results: [
+                    {
+                        item: KJ('incomplete_smingularity')
                     }
                 ]
             },
@@ -89,117 +176,66 @@ onEvent('recipes', event => {
                 type: CR('pressing'),
                 ingredients: [
                     {
-                        item: KJ('incomplete_portal_core')
+                        item: KJ('incomplete_smingularity')
                     }
                 ],
                 results: [
                     {
-                        item: KJ('incomplete_portal_core')
+                        item: KJ('incomplete_smingularity')
                     }
                 ]
             }
         ],
         results: [
             {
-                item: KJ('portal_core'),
-                chance: 100
+                item: KJ('smingularity')
             }
         ],
         loops: 1
     })
 
     event.custom({
-        type: CR('sequenced_assembly'),
-        ingredient: {
-            item: CR('sturdy_sheet')
+        type: CR('mechanical_crafting'),
+        pattern: [
+            'T',
+            'S',
+            'B'
+        ],
+        key: {
+            'T': { item: KJ('top_dimensional_casing') },
+            'S': { item: KJ('smingularity') },
+            'B': { item: KJ('bottom_dimensional_casing') }
         },
-        transitionalItem: {
-            item: KJ('incomplete_portal_core')
-        },
-        sequence: [
+        result: { item: KJ('portal_core') },
+        acceptMirrored: true
+    })
+
+    event.custom({
+        type: CR('item_application'),
+        ingredients: [
             {
-                type: CR('deploying'),
-                ingredients: [
-                    {
-                        item: KJ('incomplete_portal_core')
-                    },
-                    {
-                        item: KJ('orange_portal_component')
-                    }
-                ],
-                results: [
-                    {
-                        item: KJ('incomplete_portal_core')
-                    }
-                ]
+                item: MC('chest')
             },
             {
-                type: CR('deploying'),
-                ingredients: [
-                    {
-                        item: KJ('incomplete_portal_core')
-                    },
-                    {
-                        item: KJ('blue_portal_component')
-                    }
-                ],
-                results: [
-                    {
-                        item: KJ('incomplete_portal_core')
-                    }
-                ]
-            },
-            {
-                type: CR('pressing'),
-                ingredients: [
-                    {
-                        item: KJ('incomplete_portal_core')
-                    }
-                ],
-                results: [
-                    {
-                        item: KJ('incomplete_portal_core')
-                    }
-                ]
+                item: KJ('portal_core')
             }
         ],
         results: [
             {
-                item: 'kubejs:portal_core',
-                chance: 100
+                item: EC('ender_chest')
             }
-        ],
-        loops: 1
+        ]
     })
 
-    event.shaped(
-        EC('ender_chest'),
+    event.shapeless(
+        Item.of(EC('ender_bag'), 1), 
         [
-            'RWR',
-            'OCO',
-            'RNR'
-        ],
-        {
-            R: MC('blaze_rod'),
-            W: '#minecraft:wool',
-            O: MC('obsidian'),
-            C: KJ('portal_core'),
-            N: MC('netherite_ingot')
-        }
-    );
-
-    event.shaped(
-        EC('ender_bag'),
-        [
-            'RNR',
-            ' C ',
-            'RWR'
-        ],
-        {
-            R: MC('blaze_rod'),
-            W: '#minecraft:wool',
-            C: KJ('portal_core'),
-            N: MC('netherite_ingot')
-        }
-    );
+            {
+                item: KJ('portal_core')
+            },
+            {
+                item: 'quark:ravager_hide'
+            }
+        ]
+    )
 })
--- modpack/overrides/kubejs/startup_scripts/enderchest_items.js
+++ modpack/overrides/kubejs/startup_scripts/enderchest_items.js
@@ -1,22 +1,46 @@
 console.info('Registering custom Enderchest items');
 
 onEvent('item.registry', event => {
-	// Register new items here
-	// event.create('example_item').displayName('Example Item')
     event.create('portal_core')
         .texture(KJ('item/portal_core'))
-        .displayName('Portal Core').
-        glow(true);
+        .displayName('Portal Core')
+        .tooltip('§o§7Reality can be whatever I want...§r')
+        .rarity('epic')
+        .glow(true);
+    
+    event.create('top_dimensional_casing')
+        .texture(KJ('item/top_dimensional_casing'))
+        .displayName('Top Dimensional Casing');
 
-    event.create('incomplete_portal_core')
-        .texture(KJ('item/incomplete_portal_core'))
-        .displayName('Incomplete Portal Core');
+    event.create('bottom_dimensional_casing')
+        .texture(KJ('item/bottom_dimensional_casing'))
+        .displayName('Bottom Dimensional Casing');
 
-    event.create('blue_portal_component')
-        .texture(KJ('item/blue_portal_component'))
-        .displayName('Blue Portal Component');
+    event.create('smingularity')
+        .texture(KJ('item/smingularity'))
+        .displayName('S\'mingularity')
+        .tooltip('§o§7Surprisingly inedible.§r')
+        .rarity('rare')
+        .glow(true);
 
-    event.create('orange_portal_component')
-        .texture(KJ('item/orange_portal_component'))
-        .displayName('Orange Portal Component');
+    event.create('incomplete_smingularity')
+        .texture(KJ('item/incomplete_smingularity'))
+        .displayName('Incomplete S\'mingularity');
+
+    event.create('graham_cracker')
+        .texture(KJ('item/graham_cracker'))
+        .displayName('Graham Cracker');
+
+    event.create('bar_of_darkest_chocolate')
+        .texture(KJ('item/bar_of_darkest_chocolate'))
+        .displayName('Bar of Darkest Chocolate');
+})
+
+onEvent('fluid.registry', event => {
+    event.create('darkest_chocolate')
+        .thickTexture(0x1a1106)
+        .displayName('Darkest Chocolate')
+        .noBucket()
+        .noBlock()
+        .rarity('epic')
 })
